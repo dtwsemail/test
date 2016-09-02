77331eb3ecf615b45e6b0b31fa5bd73957bed8c0
dtwsemail <dtwsemail@163.com>
2016-09-02 14:48:01 +0800

    hehe

--- src/main/java/stock/history/controller/Main.java
+++ src/main/java/stock/history/controller/Main.java
@@ -14,5 +14,11 @@ import static sun.net.www.protocol.http.HttpURLConnection.userAgent;
 
 public class Main {
 
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        System.out.println("hello git");
+
+    }
 
 }
